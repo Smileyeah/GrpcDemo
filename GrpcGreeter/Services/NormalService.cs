fd8cd21af8848e7a75c31d08319b7840841d51ec
haogeer <lihao@ganweisoft.com>
2021-01-28 16:07:07 +0800

    feat:first commit

---
+++ GrpcGreeter/Services/NormalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcGreeter
+{
+	public class NormalService : Normal.NormalBase
+	{
+		private readonly ILogger<NormalService> _logger;
+		public NormalService(ILogger<NormalService> logger)
+		{
+			_logger = logger;
+		}
+
+		public override Task<Actors> GetActors(Google.Protobuf.WellKnownTypes.Empty request, ServerCallContext context)
+        {
+			return Task.FromResult(new Actors
+			{
+				Actor = "Connell",
+				Actress = "Marian"
+			});
+		}
+	}
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient1" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            
+            var heyaReply = await client.SayHeyaAsync(
+                              new HelloRequest { Name = "GreeterClient2" });
+            Console.WriteLine("Greeting: " + heyaReply.Message);
+
+            
+            var normalClient =  new Normal.NormalClient(channel);
+            var normalReply = await normalClient.GetActorsAsync(
+                              new Google.Protobuf.WellKnownTypes.Empty());
+            Console.WriteLine("Actor: " + normalReply.Actor + Environment.NewLine + "Actress: " + normalReply.Actress);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
